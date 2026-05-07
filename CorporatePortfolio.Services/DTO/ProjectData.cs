7af0bdefb157292177b1a2e27d0f5552358488eb
dave.w.turner <dave.w.turner@outlook.com>
2026-05-07 04:05:33 -0400

    Updates for projects on main page and small visual tweaks. Resume updated with project and link.

---
+++ CorporatePortfolio.Services/DTO/ProjectData.cs
@@ -0,0 +1,11 @@
+﻿namespace CorporatePortfolio.Services.DTO
+{
+    public class ProjectData
+    {
+        public string Title { get; set; }
+        public string Date { get; set; }
+        public string SourceUrl { get; set; }
+        public List<string> Details { get; set; } = [];
+        public List<string> Tags { get; set; } = [];
+    }
+}
